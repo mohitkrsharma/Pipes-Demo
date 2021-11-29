e27407401959584ed8fb2f5325c779a47faba1d1
mohitkumarsharma <59123602+mohitkrsharma@users.noreply.github.com>
2021-11-29 10:52:59 +0530

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MultiplierPipe } from './multiplier.pipe';
+
+@NgModule({
+  declarations: [AppComponent, MultiplierPipe],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/multiplier.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'multiplier',
+})
+export class MultiplierPipe implements PipeTransform {
+  transform(value: number, multiply: number): number {
+    return multiply * value;
+  }
+}
